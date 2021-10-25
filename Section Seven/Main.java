e903f74b5e0a7f1d05763447e1ad7c90ed8a05be
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2021-10-25 18:00:55 -0500

    Add/change multiple files in Section Seven and Workbooks & Challenges folders

--- Section Seven/Main.java
+++ Section Seven/Main.java
@@ -2,17 +2,21 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        String[] parts = {"tires", "keys"};
-        Car nissan = new Car("Nissan", 5000, 2020, "red", parts);
-        Car dodge = new Car("Dodge", 8500, 2019, "blue", parts);
-        Car nissan2 = new Car(nissan);
 
-        nissan2.setColor("yellow");
-        nissan2.setParts(new String[] {"tires", "filter"}); 
+        Car[] cars = new Car[] {
+            new Car("Nissan", 5000, 2020, "red", new String[] {"tires", "keys"}),
+            new Car("Dodge", 8500, 2019, "blue", new String[] {"tires", "keys"}),
+            new Car("Nissan", 5000, 2017, "yellow", new String[] {"tires", "filter"})
 
-        System.out.println(nissan);
-        System.out.println(dodge);
-        System.out.println(nissan2);
+        };
+    
+
+        Dealership dealership = new Dealership();
+
+        for (int i = 0; i < cars.length; i++) {
+            dealership.setCar(cars[i], i);
+        }
+
 
     }
 }
