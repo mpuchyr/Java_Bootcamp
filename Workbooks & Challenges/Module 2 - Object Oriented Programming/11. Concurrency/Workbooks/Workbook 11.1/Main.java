a6b3058c2d75f6f548facdfba5b6278170def203
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2021-12-07 16:40:31 -0600

    Continue on Workbook 1.1 in Concurrency section

--- Workbooks & Challenges/Module 2 - Object Oriented Programming/11. Concurrency/Workbooks/Workbook 11.1/Main.java
+++ Workbooks & Challenges/Module 2 - Object Oriented Programming/11. Concurrency/Workbooks/Workbook 11.1/Main.java
@@ -8,24 +8,45 @@ import java.util.Scanner;
 public class Main {
 
     static final String SALES = "data/sales.csv"; //Use backslash Windows users
+    static double furnitureAverage;
+    static double technologyAverage;
+    static double officeSuppliesAverage;
+    static double total;
 
     public static void main(String[] args) {
         
         try {
             Path path = Paths.get(Thread.currentThread().getContextClassLoader().getResource(SALES).toURI());
-            Thread thread2 = new Thread(() -> {
-                average(path, "Furniture");
-                average(path, "Technology");
-                average(path, "Office Supplies");
-                totalAverage(path);
-            });
+            Thread thread2 = new Thread(() -> furnitureAverage = average(path, "Furniture"));
+            Thread thread3 = new Thread(() -> technologyAverage = average(path, "Technology"));
+            Thread thread4 = new Thread(() -> officeSuppliesAverage = average(path, "Office Supplies"));
+            Thread thread5 = new Thread(() -> total = totalAverage(path));
+
             thread2.start();
+            thread3.start();
+            thread4.start();
+            thread5.start();
+
 
 
             Scanner scan = new Scanner(System.in);
             System.out.print("Please enter your name to access the Global Superstore dataset: ");
             String name = scan.nextLine();
-            System.out.println("Access Denied. We apologize for the inconvenience. Have a good day " + name + ".");
+            try {
+                thread2.join();
+                thread3.join();
+                thread4.join();
+                thread5.join();
+
+                System.out.println("\nThank you " + name + ". The average sales for Global Superstore are:\n");
+                System.out.println("Average Furniture Sales: " + furnitureAverage);
+                System.out.println("Average Technology Sales: " + technologyAverage);
+                System.out.println("Average Office Supplies Sales: " + officeSuppliesAverage);
+                System.out.println("Total Average: " + total);
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+
             scan.close();
         } catch (URISyntaxException e) {
             System.out.println(e.getMessage());
@@ -37,7 +58,7 @@ public class Main {
         
         try {
             return Files.lines(path)
-            .skip(0)
+            .skip(1)
             .map((line) -> line.toString().split(","))
             .filter((line) -> line[0].equals(category))
             .mapToDouble((line) -> Double.parseDouble(line[1]) * Double.parseDouble(line[2]))
@@ -55,7 +76,7 @@ public class Main {
      public static Double totalAverage(Path path){
         try {
             return Files.lines(path)
-            .skip(0)
+            .skip(1)
             .map((line) -> line.toString().split(","))
             .mapToDouble((line) -> Double.parseDouble(line[1]) * Double.parseDouble(line[2]))
             .average()
--- Workbooks & Challenges/Module 2 - Object Oriented Programming/11. Concurrency/joining-threads/Main.java
+++ Workbooks & Challenges/Module 2 - Object Oriented Programming/11. Concurrency/joining-threads/Main.java
@@ -1,24 +1,45 @@
+import java.util.Scanner;
+
 public class Main {
 
     static final double TARGET = 0.5;
-    static final double PRECISION = 0.000000001;
-
+    static final double PRECISION = 0.000000001; 
+    static double result = 0;
     public static void main(String[] args) {
 
-        //call generateNumber here...
-        //calculate precision level here...
-        System.out.println("The computer returned a value of: " + <result>);
-        System.out.println("The value was generated to a precision of : " + <precision>);
+        Runnable runnable = () -> result = generateNumber();
+        Thread thread2 = new Thread(runnable);
+        thread2.start();
+        double precision = difference(result);
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a name to generate a number: ");
+        scan.nextLine();
+        try {
+            thread2.join();
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("The computer returned a value of: " + result);
+        System.out.println("The value was generated to a precision of : " + precision);
+
+        scan.close();
     }
 
-    /**
-     * Function name: generateNumber
-     * @return double
-     * 
-     * Inside the function:
-     *   1. Generates a number close to the TARGET to a precision of PRECISION.
-     * 
-     */
+     public static double generateNumber() {
+         double number = Math.random();
+         double difference = difference(number);
+         while (difference > PRECISION) {
+             number = Math.random();
+            difference = difference(number);
+         }
+
+         return difference;
+     }
+
+     public static double difference(double number) {
+        return Math.abs(TARGET - number);
+     }
 
 
 }
