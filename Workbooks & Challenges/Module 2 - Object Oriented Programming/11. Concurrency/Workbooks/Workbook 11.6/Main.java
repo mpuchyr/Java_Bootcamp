7896466b7adef87e3a36308a38d1991493b22e7e
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2021-12-09 16:20:20 -0600

    Complete Workbook 11.6 in Concurrency

--- Workbooks & Challenges/Module 2 - Object Oriented Programming/11. Concurrency/Workbooks/Workbook 11.6/Main.java
+++ Workbooks & Challenges/Module 2 - Object Oriented Programming/11. Concurrency/Workbooks/Workbook 11.6/Main.java
@@ -1,4 +1,11 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
  
@@ -11,14 +18,47 @@ public class Main {
     public static void main(String[] args) throws Exception {
      
             // execute tasks here
-     
+            int numOfThreads = Runtime.getRuntime().availableProcessors();
+            CountDownLatch latch = new CountDownLatch(files.length);
+            ReentrantLock lock = new ReentrantLock();
+            ExecutorService executor = Executors.newFixedThreadPool(numOfThreads);
+            for (String string : files) {
+                executor.submit(() -> increment(string, latch, lock));
+            }
+
             Scanner scan = new Scanner(System.in);
             System.out.print("Please enter your name to access the Global Superstore data: ");
             String name = scan.nextLine();
             System.out.println("\nThank you " + name + ".\n");
             scan.close();
+
+            latch.await();
+            executor.shutdown();
+
+            System.out.println("Sample size: " + sampleSize);
+            System.out.println("Quantity Sold: " + quantitySold);
             
      
+    }
+
+    public static void increment (String file, CountDownLatch latch, ReentrantLock lock) {     
+        try {
+            Path path = Paths.get(Thread.currentThread().getContextClassLoader().getResource(file).toURI());
+            Files.lines(path)
+            .skip(1)
+            .mapToInt((line) -> Integer.parseInt(line.toString().split(",")[2]))
+            .forEach((quantity) -> {
+                lock.lock();
+                quantitySold += quantity;
+                sampleSize++;
+                lock.unlock();
+            });
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        latch.countDown();
+
     }
 
 }
--- Workbooks & Challenges/Module 2 - Object Oriented Programming/11. Concurrency/reentrant-lock/Main.java
+++ Workbooks & Challenges/Module 2 - Object Oriented Programming/11. Concurrency/reentrant-lock/Main.java
@@ -1,16 +1,43 @@
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Main {
     
     static int counter = 0;
     
     public static void main(String[] args) {
-    
+        
+        int numOfThreads = Runtime.getRuntime().availableProcessors();
+        ExecutorService executor = Executors.newFixedThreadPool(numOfThreads);
+        CountDownLatch latch = new CountDownLatch(55);
+        ReentrantLock lock = new ReentrantLock();
+        for (int i = 0; i < 55; i++) {
+            executor.submit(() -> task(lock, latch));
+        }
+
+
+        try {
+            latch.await();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            executor.shutdown();
+        }
+
+
+        System.out.println(counter);
     }
     
-    public static void task() {
+    public static void task(ReentrantLock lock, CountDownLatch latch) {
         //some really long operations that need to run in the background...
         for (int i = 0; i < 10000; i++) {
+            lock.lock();
             counter++; // there happens to be a line that updates the counter variable. 
+            lock.unlock();
         }
+        latch.countDown();
         //more really long operations...
     }
   
