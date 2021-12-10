1505e3dc71ddbd3bd7557fa547e286724821f401
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2021-12-10 15:38:48 -0600

    Complete final workbook in Concurrency

--- Workbooks & Challenges/Module 2 - Object Oriented Programming/11. Concurrency/Workbooks/Workbook 11.6/Main.java
+++ Workbooks & Challenges/Module 2 - Object Oriented Programming/11. Concurrency/Workbooks/Workbook 11.6/Main.java
@@ -5,14 +5,15 @@ import java.util.Scanner;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
  
     static String[] files = new String[] { "data/sales1.csv", "data/sales2.csv", "data/sales3.csv"};
    
-    static int sampleSize = 0;
-    static int quantitySold = 0;
+
+    static AtomicInteger sampleSize = new AtomicInteger(0);
+    static AtomicInteger quantitySold = new AtomicInteger(0);
 
 
     public static void main(String[] args) throws Exception {
@@ -20,10 +21,9 @@ public class Main {
             // execute tasks here
             int numOfThreads = Runtime.getRuntime().availableProcessors();
             CountDownLatch latch = new CountDownLatch(files.length);
-            ReentrantLock lock = new ReentrantLock();
             ExecutorService executor = Executors.newFixedThreadPool(numOfThreads);
             for (String string : files) {
-                executor.submit(() -> increment(string, latch, lock));
+                executor.submit(() -> increment(string, latch));
             }
 
             Scanner scan = new Scanner(System.in);
@@ -41,17 +41,15 @@ public class Main {
      
     }
 
-    public static void increment (String file, CountDownLatch latch, ReentrantLock lock) {     
+    public static void increment (String file, CountDownLatch latch) {     
         try {
             Path path = Paths.get(Thread.currentThread().getContextClassLoader().getResource(file).toURI());
             Files.lines(path)
             .skip(1)
             .mapToInt((line) -> Integer.parseInt(line.toString().split(",")[2]))
             .forEach((quantity) -> {
-                lock.lock();
-                quantitySold += quantity;
-                sampleSize++;
-                lock.unlock();
+                quantitySold.getAndAdd(quantity);
+                sampleSize.getAndAdd(1);
             });
         } catch (Exception e) {
             System.out.println(e.getMessage());
--- Workbooks & Challenges/Module 2 - Object Oriented Programming/11. Concurrency/reentrant-lock/Main.java
+++ Workbooks & Challenges/Module 2 - Object Oriented Programming/11. Concurrency/reentrant-lock/Main.java
@@ -1,20 +1,20 @@
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
     
-    static int counter = 0;
+    static AtomicInteger counter = new AtomicInteger(0);
     
     public static void main(String[] args) {
         
         int numOfThreads = Runtime.getRuntime().availableProcessors();
         ExecutorService executor = Executors.newFixedThreadPool(numOfThreads);
         CountDownLatch latch = new CountDownLatch(55);
-        ReentrantLock lock = new ReentrantLock();
         for (int i = 0; i < 55; i++) {
-            executor.submit(() -> task(lock, latch));
+            executor.submit(() -> task(latch));
         }
 
 
@@ -30,12 +30,10 @@ public class Main {
         System.out.println(counter);
     }
     
-    public static void task(ReentrantLock lock, CountDownLatch latch) {
+    public static void task(CountDownLatch latch) {
         //some really long operations that need to run in the background...
         for (int i = 0; i < 10000; i++) {
-            lock.lock();
-            counter++; // there happens to be a line that updates the counter variable. 
-            lock.unlock();
+            counter.addAndGet(1); // there happens to be a line that updates the counter variable. 
         }
         latch.countDown();
         //more really long operations...
