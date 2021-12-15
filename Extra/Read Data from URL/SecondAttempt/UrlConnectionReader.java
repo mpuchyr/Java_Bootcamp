9f97d8a62c49a3e3cbaacbed0469d67860e78014
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2021-12-15 15:27:21 -0600

    Update UrlConnectionReader in SecondAttempt in Extra to allow user to enter the url

--- Extra/Read Data from URL/SecondAttempt/UrlConnectionReader.java
+++ Extra/Read Data from URL/SecondAttempt/UrlConnectionReader.java
@@ -1,10 +1,19 @@
 import java.net.*;
+import java.util.Scanner;
 import java.io.*;
 
 public class UrlConnectionReader {
     public static void main(String[] args) {
-        String output = getUrlContents("https://javatpoint.com/java-tutorial");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What url would you like to get the contents for? ");
+        String url = scan.nextLine();
+
+        String output = getUrlContents(url);
         System.out.println(output);
+
+        scan.close();
+        // String output = getUrlContents("https://javatpoint.com/java-tutorial");
+        // System.out.println(output);
     }
 
     private static String getUrlContents(String theUrl) {
