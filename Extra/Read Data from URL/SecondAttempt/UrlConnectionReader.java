aef5e9ca3fb8cc7413a45305691d2d127a19f0d8
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2021-12-16 14:19:58 -0600

    Wrap URL retrieval in a loop in UrlConnectionReader.java

--- Extra/Read Data from URL/SecondAttempt/UrlConnectionReader.java
+++ Extra/Read Data from URL/SecondAttempt/UrlConnectionReader.java
@@ -5,11 +5,28 @@ import java.io.*;
 public class UrlConnectionReader {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("What url would you like to get the contents for? ");
-        String url = scan.nextLine();
 
-        String output = getUrlContents(url);
-        System.out.println(output);
+        boolean running = true;
+
+        while (running) {
+            System.out.println("What url would you like to get the contents for? ");
+            String url = scan.nextLine();
+    
+            String output = getUrlContents(url);
+            System.out.println(output);
+
+            System.out.println("Would you like to get contents for another url? yes or no");
+            String answer = scan.nextLine();
+            while (!(answer.equals("yes") || answer.equals("no"))) {
+                System.out.println("Answer must be yes or no");
+                answer = scan.nextLine();
+            }
+            if (answer.equals("no")) {
+                break;
+            }
+        }
+
+
 
         scan.close();
         // String output = getUrlContents("https://javatpoint.com/java-tutorial");
