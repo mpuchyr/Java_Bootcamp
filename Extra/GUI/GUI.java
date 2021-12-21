939f238982e03b8010803771c6fb4a3d60bf17ee
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2021-12-21 15:13:53 -0600

    Add more buttons to GUI.java in GUI

--- Extra/GUI/GUI.java
+++ Extra/GUI/GUI.java
@@ -8,10 +8,16 @@ class GUI {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300, 300);
         frame.setLayout(new BorderLayout());
-        JButton button = new JButton("Button 1");
-        JButton button2 = new JButton("Button 2");
+        JButton button = new JButton("West");
+        JButton button2 = new JButton("East");
+        JButton button3 = new JButton("Center");
+        JButton button4 = new JButton("North");
+        JButton button5 = new JButton("South");
         frame.add(button, BorderLayout.WEST);
         frame.add(button2, BorderLayout.EAST);
+        frame.add(button3, BorderLayout.CENTER);
+        frame.add(button4, BorderLayout.NORTH);
+        frame.add(button5, BorderLayout.SOUTH);
         frame.setVisible(true);
     }
 }
